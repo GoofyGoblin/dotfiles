e6a9d516f2327da7560d218fcc43935771ec672a
winter <hoh20677@gmail.com>
2026-02-19 22:07:46 +0700

    stuff

--- change_dunst_colors.sh
+++ change_dunst_colors.sh
@@ -5,9 +5,9 @@ source ~/.cache/wal/colors.sh
 cat > ~/.config/dunst/dunstrc.tmp << EOF
 [global]
     geometry = "300x100-10+10"
-	foreground = $color7
-	background = $color0
-	border = $color1
+	foreground = "$color7"
+	background = "$color0"
+	border = "$color1"
 	font = "JetBrainsMonoNL 12"
 # You can add more Dunst-specific configurations here using the imported colors
 EOF
