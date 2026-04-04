1a97478620be0c6dd104c96b4f0f54e26ce87316
winter <hoh20677@gmail.com>
2026-04-04 20:49:27 +0700

    removed weird symlinked dir

--- .config/nvimbak/init.lua
+++
@@ -1,7 +0,0 @@
-require("config.lazy")
-require("config")
-require("config.set")
-vim.o.wrap = true
-vim.o.linebreak = true
-vim.o.breakindent = true
-vim.o.clipboard = "unnamedplus"
--- .config/nvimbak/lua/config/init.lua
+++
@@ -1,2 +0,0 @@
-require("config.remap")
-vim.opt.clipboard = "unnamedplus"
